4fec2dcf9d0a2d73a2023b7c0c82ee5b18735ff0
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-10-09 17:18:39 -0700

    Added a couple icons.

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -2,13 +2,19 @@ import React from "react";
 import "./navbar.css";
 import StrykerLogo from "./StrykerLogo";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import AssignmentTwoToneIcon from "@material-ui/icons/AssignmentTwoTone";
 
 function Navbar() {
   return (
     <div className='navbar'>
       <StrykerLogo />
-      <div className='account-circle-icon'>
-        <AccountCircleIcon fontSize='large' />
+      <div className='icons-container'>
+        <div className='assignment-two-tone-icon'>
+          <AssignmentTwoToneIcon fontSize='large' />
+        </div>
+        <div className='account-circle-icon'>
+          <AccountCircleIcon fontSize='large' />
+        </div>
       </div>
     </div>
   );
